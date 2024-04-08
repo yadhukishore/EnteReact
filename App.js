4306471a3800d5955de9e367e401293da2bb9bff
Yadhukrishna <yadhukishore37@gmail.com>
2024-04-08 16:30:39 +0530

    Ignited My Create App

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 
 const parent = React.createElement("div",{id:"parent"},
 [React.createElement("div",{id:"child"},
 [React.createElement("h1",{},"Hi iam yadhu H1 tag"),
-React.createElement("h2",{},"Hi iam yadhu H2 tag")
+React.createElement("h2",{},"Hi iam yadhu H2 tagzzzzz")
 ]
 ),
 
 React.createElement("div",{id:"child2"},
 [React.createElement("h1",{},"Hi iam yadhu H1 tag from child2"),
-React.createElement("h2",{},"Hi iam yadhu H2 tag Child2")
+React.createElement("h2",{},"Hi iam yadhu H2 fhakjfh Child2")
 ]
 )]
 );
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # React Learning
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacemrnt
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- IMAGE Optimization
+- Minification
+- Bundling
+- Compressing
