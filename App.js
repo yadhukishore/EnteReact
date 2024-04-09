07a534caf48966ac55efd984efb5e7fe6b0ee29a
Yadhukrishna <yadhukishore37@gmail.com>
2024-04-09 12:44:21 +0530

    Foundations

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-
-const parent = React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"Hi iam yadhu H1 tag"),
-React.createElement("h2",{},"Hi iam yadhu H2 tagzzzzz")
-]
-),
-
-React.createElement("div",{id:"child2"},
-[React.createElement("h1",{},"Hi iam yadhu H1 tag from child2"),
-React.createElement("h2",{},"Hi iam yadhu H2 fhakjfh Child2")
-]
-)]
+const nestedHeader = React.createElement(
+  "div",
+  { className: "title" },
+  React.createElement("h1", null, "Main Header"),
+  React.createElement("h2", null, "SubHeader")
 );
 
+const jsxNestedHeading = (
+  <div className="title">
+    <h1>Main Header</h1>
+    <h2>SubHeader</h2>
+  </div>
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "Not Rendered");
+
+function RandFunc(){return(
+  <h1>Hey Ramdom function$</h1>
+);}
+const Sasiye=1000;
+const elem = <h2>Element</h2>
+// JSX
+const JsxHeading = () => {
+    return (
+    <div>
+      <RandFunc/>
+      <h1>{Sasiye+200}</h1>
+      {elem}
+   <h1 id="heading">Nof Renterd in side a Functionnn!</h1>, <h2>H2 aane</h2>
+  </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<JsxHeading/>);
